d8e5e654d45737c9ec85846a7689198053632bfa
SalikSayyed <51844798+SalikSayyed@users.noreply.github.com>
2021-03-30 18:22:35 +0530

    Second Commit Salik

--- ccpp/ccpp/views.py
+++ ccpp/ccpp/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 def index(request):
-    #return render(request,'index.html')
-    return HttpResponse ('Hello Rupa Mavshi')
+    # return render(request,'index.html')
+    return HttpResponse('<h1>Hello World</h1><hr><marquee style="color: red;font-size: 40px">Working Project</marquee>')
+    
