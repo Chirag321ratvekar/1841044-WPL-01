0ef0b627f4779513093f11b1da878039e5d662c7
SalikSayyed <51844798+SalikSayyed@users.noreply.github.com>
2021-03-30 19:27:14 +0530

    Set View commit

--- ccpp/ccpp/views.py
+++ ccpp/ccpp/views.py
@@ -4,5 +4,5 @@ from django.shortcuts import render
 
 def index(request):
     # return render(request,'index.html')
-    return HttpResponse('<h1>Hello World</h1><hr><marquee style="color: red;font-size: 40px">Working Project</marquee>')
+    return HttpResponse('<h1 style="font-family:aerial;">Hello World</h1><marquee>!!!Project working!!!</marquee>')
     
